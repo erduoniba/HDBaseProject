bb4617306bca25e86ae28fd769bec0e108f99ea3
denglibing <13049862397@163.com>
2020-08-22 14:57:00 +0800

    pod 发布错误问题修正

--- Example/_pods/HDBaseProject/HDGlobalMethods/HDNetStatusManager.h
+++ Example/_pods/HDBaseProject/HDGlobalMethods/HDNetStatusManager.h
@@ -8,7 +8,11 @@
 
 #import <Foundation/Foundation.h>
 
+#if __has_include(<AFNetworking/AFNetworking.h>)
 #import <AFNetworking/AFNetworking.h>
+#else
+#import "AFNetworking.h"
+#endif
 
 NS_ASSUME_NONNULL_BEGIN
 
